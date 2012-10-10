f51f459128d29d226f333b3f75f884b7d949cd37
John Lato <jwlato@tsurucapital.com>
2012-10-10 16:15:53 +0800

    add some argument handling to control verbosity

--- ConstMath/Plugin.hs
+++ ConstMath/Plugin.hs
@@ -2,6 +2,7 @@ module ConstMath.Plugin (
       plugin -- :: Plugin
 ) where
 
+import ConstMath.Types
 import ConstMath.Pass (constMathProgram)
 import GhcPlugins
 
@@ -13,8 +14,19 @@ plugin = defaultPlugin {
   }
 
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
-install _ todos = do
+install args todos = do
     reinitializeGlobals
     let pass = CoreDoPasses [constMath]
     return $ intersperse pass todos
-  where constMath = CoreDoPluginPass "Constant Math Elimination" (bindsOnlyPass constMathProgram)
+  where constMath = CoreDoPluginPass "Constant Math Elimination"
+            (bindsOnlyPass (constMathProgram (parseOpts args)) )
+
+-- TODO: use a real parser
+parseOpts :: [CommandLineOption] -> Opts
+parseOpts = foldr ($) defaultOpts . map mkArg
+    where
+        mkArg flag
+            | flag `elem` ["-v","--verbose","--verbosity=1"]    = setVerbosity Basic
+            | flag `elem` ["-v11", "-verbosity=11","--trace"] = setVerbosity Trace
+            | flag `elem` ["-q", "--quiet","--verbosity=0", "-v0"]  = setVerbosity None
+            | otherwise = id
