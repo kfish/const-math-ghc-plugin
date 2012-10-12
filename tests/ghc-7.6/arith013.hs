afbbd6c2b2bcc107d95c1bc7129a019dba69e617
Conrad Parker <conrad@metadecks.org>
2012-10-12 14:10:18 +0800

    tests: import ghc-7.6 numeric testsuite

---
+++ tests/ghc-7.6/arith013.hs
@@ -0,0 +1,13 @@
+-- Test gcdInt/gcdInteger
+
+import GHC.Real ( gcdInt )
+import GHC.Integer.GMP.Internals ( gcdInteger )
+
+main :: IO ()
+main = do
+   test gcdInt     [                       -42, 0, 105             ]
+   test gcdInteger [-12193263111263526900, -42, 0, 105, 1234567890 ]
+
+
+test :: (Show a, Integral a) => (a -> a -> a) -> [a] -> IO ()
+test f xs = mapM_ print [ (a, b, f a b) | a <- xs, b <- reverse xs, a /= 0  || b /= 0 ]
